cbc677d0bcb49b8fff430c1fcc2862f705ea033b
Osman Abdelmukaram <62990465+osman-abdelmukaram@users.noreply.github.com>
2021-01-20 18:02:43 +0100

    Add files via upload

---
+++ Assignment3/server.js
@@ -0,0 +1,107 @@
+const sqlite = require('sqlite3').verbose();
+let db = my_database('./products.db');
+
+var express = require("express");
+var app = express();
+let router = express.Router();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+
+router.get("/items", function(req, res) {
+
+    let sql = "SELECT * from products"
+
+    db.all(sql, [], function(err, rows) {
+        if (err) throw err;
+
+        res.json(rows)
+    } )
+
+    // Response gets converted to JSON and sent further
+    // res.json()
+})
+
+
+router.get("/items/", function(req, res) {
+
+
+
+
+    // Response gets converted to JSON and sent further
+    res.json()
+})
+
+
+router.post("/items", function(req, res) {
+
+    console.log(req.body)
+
+
+    // Response gets converted to JSON and sent further
+    res.json(req.body)
+})
+
+
+router.put("/items", function(req, res) {
+
+
+
+
+    // Response gets converted to JSON and sent further
+    res.json()
+})
+
+
+router.delete("/items", function(req, res) {
+
+
+
+
+    // Response gets converted to JSON and sent further
+    res.json()
+})
+
+
+
+app.use("/api", router);
+
+
+app.listen(3000);
+console.log("Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/hello");
+
+
+// ###############################################################################
+// Some helper functions called above
+function my_database(filename) {
+    // Conncect to db by opening filename, create filename if it does not exist:
+    var db = new sqlite.Database(filename, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Connected to the products database.');
+    });
+    // Create our products table if it does not exist already:
+    db.serialize(() => {
+        db.run(`
+        	CREATE TABLE IF NOT EXISTS products
+        	(id 	  INTEGER PRIMARY KEY,
+        	product	CHAR(100) NOT NULL,
+        	origin 	CHAR(100) NOT NULL,
+        	best_before_date 	CHAR(20) NOT NULL,
+          amount  CHAR(20) NOT NULL,
+        	image   CHAR(254) NOT NULL
+        	)`);
+        db.all(`select count(*) as count from products`, function (err, result) {
+            if (result[0].count == 0) {
+                db.run(`INSERT INTO products (product, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)`,
+                    ["Apples", "The Netherlands", "November 2019", "100kg", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Apples.jpg/512px-Apples.jpg"]);
+                console.log('Inserted dummy Apples entry into empty product database');
+            } else {
+                console.log("Database already contains", result[0].count, " item(s) at startup.");
+            }
+        });
+    });
+    return db;
+}
