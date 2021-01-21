bd70f4dbec782f9fd2f5ca7d38c3d5325182af5d
[Osman Abdelmukaram] <[osman.abdelmukaram]>
2021-01-21 04:11:51 +0100

    Created CRUD routes, error handling;

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -6,6 +6,9 @@ const app = express();
 const router = express.Router();
 
 const bodyParser = require("body-parser");
+const {
+    json
+} = require('body-parser');
 app.use(bodyParser.json());
 
 
@@ -16,7 +19,12 @@ app.use(bodyParser.json());
 router.get("/", function (req, res) {
 
     db.all("SELECT id, product, origin, best_before_date, amount, image FROM products", function (err, rows) {
-
+        // check error handling with TA
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.json(rows);
+        }
     })
 
 });
@@ -27,43 +35,57 @@ router.get("/:id", function (req, res) {
     let id = req.params.id;
 
     db.all("SELECT id, product, origin, best_before_date, amount, image FROM products WHERE id=" + id, function (err, row) {
-
+        // check error handling with TA
+        if (err) {
+            res.status(400).send(err);
+        } else if (JSON.stringify(row) == "[]") {
+            res.status(404).send(err)
+        } else {
+            res.json(row);
+        }
     })
-
-
 });
 
 // POSTING ITEM 
 router.post("/", function (req, res) {
 
-    // Might be wrong
     let item = req.body;
 
-    db.run(`INSERT INTO products (product, origin, best_before_date, amount, image)
-    VALUES (?, ?, ?, ?, ?)`,
+    console.log(item);
+
+    db.run(`INSERT INTO products (product, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)`,
         [item['product'], item['origin'], item['best_before_date'], item['amount'], item['image']],
-        function (err, row) {
-
+        function (err, item) {
+            // check error handling with TA
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.status(201).json(item);
+            }
+            res.send(row);
         })
-
-
 });
 
 // UPDATING ITEM
 router.put("/", function (req, res) {
 
-    // Might be wrong
     let item = req.body;
 
-    db.run(`UPDATE products
-    SET product=?, origin=?, best_before_date=?, amount=?,
-    image=? WHERE id=?`,
+    // is it fine if we require all items info to be requested with??? 
+    db.run(`UPDATE products SET product=?, origin=?, best_before_date=?, amount=?,mage=? WHERE id=?`,
         [item['product'], item['origin'], item['best_before_date'], item['amount'], item['image'], item['id']],
-        function (err, row) {
-
+        function (err, item) {
+            if (err) console.log(err);
+            // check error handling with TA
+            if (err) {
+                res.status(400).send(err);
+            } else if (item.n === 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(204);
+                //  Might want to respond with the updated version of item
+            }
         })
-
-
 });
 
 // DELETING ITEM
@@ -71,14 +93,30 @@ router.delete("/:id", function (req, res) {
 
     let id = req.params.id
 
-    db.run("DELETE FROM products WHERE id=" + id, function (err) {
+    db.run("DELETE FROM products WHERE id=" + id, function (err, item) {
+        // check error handling with TA
         if (err) {
-            throw err;
+            res.status(400).send(err);
+        } else if (item.n === 0) {
+            res.sendStatus(404);
+        } else {
+            res.sendStatus(204);
+            // Might want to inform somehow item was deleted
         }
     })
-
 });
 
+// RESET ITEM - doesn't work
+router.get("/reset", function (req, res) {
+
+    console.log("Hitting it")
+
+    db.run("DELETE FROM products", function (err) {
+        if (err) console.log(err);
+        // ERROR HANDLING
+        res.send("Table reset")
+    })
+});
 
 app.use("/api/items", router);
 
@@ -92,14 +130,15 @@ console.log("Your Web server should be up and running, waiting for requests to c
 
 // INIT DATABASE
 function my_database(filename) {
-    // Conncect to db by opening filename, create filename if it does not exist:
+    // Connects to db by opening filename, creates filename if it does not exist:
     let db = new sqlite.Database(filename, (err) => {
         if (err) {
             console.error(err.message);
         }
         console.log('Connected to the products database.');
     });
-    // Create our products table if it does not exist already:
+
+    // Creates products table if it does not exist already:
     db.serialize(() => {
         db.run(`
         	CREATE TABLE IF NOT EXISTS products
@@ -109,9 +148,11 @@ function my_database(filename) {
         	best_before_date 	CHAR(20) NOT NULL,
           amount  CHAR(20) NOT NULL,
         	image   CHAR(254) NOT NULL
-        	)`);
+            )`);
+
         db.all(`select count(*) as count from products`, function (err, result) {
             if (result[0].count == 0) {
+                // Generates first item in the products table
                 db.run(`INSERT INTO products (product, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)`,
                     ["Apples", "The Netherlands", "November 2019", "100kg", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Apples.jpg/512px-Apples.jpg"]);
                 console.log('Inserted dummy Apples entry into empty product database');
