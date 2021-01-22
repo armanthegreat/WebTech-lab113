20084a9e6176320dd789af02ad6cc424e3ffabf9
[Osman Abdelmukaram] <[osman.abdelmukaram]>
2021-01-22 22:23:06 +0100

    Restructured document;

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -8,27 +8,37 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-//SETTING HEADER FOR ALL ROUTES - !!!!
-// app.use(res.set({
-//     "content-type": "application/json",
-// }));
+//HTTP HEADER MIDDLEWARE
+app.use(function(req, res, next) {
+   res.set({
+       "content-type": "application/json"
+    //    Check what headers TA mentioned and add them;
+   });
+    next();
+})
 
 // ************************************************************************************************ //
 //                                       ROUTERS                                                    //
 
-// GETTING ALL ITEMS
-router.get("/items", function (req, res) {
+// Create
+router.post("/items", function (req, res) {
 
-    db.all("SELECT id, product, origin, best_before_date, amount, image FROM products", function (err, rows) {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            res.json(rows);
-        }
-    })
+    let item = req.body;
+
+    db.run(`INSERT INTO products (product, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)`,
+        [item['product'], item['origin'], item['best_before_date'], item['amount'], item['image']],
+        function (err, item) {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.status(201).json(item);
+                // res.send(row);
+                // might want to respond to the URI of the new item
+            }
+        })
 });
 
-// GETTING PARTICULAR ITEM WITH ID
+// Read (single item)
 router.get("/items/:id", function (req, res) {
 
     let id = req.params.id;
@@ -44,89 +54,78 @@ router.get("/items/:id", function (req, res) {
     })
 });
 
-// POSTING ITEM 
-router.post("/items", function (req, res) {
+// Read (all items)
+router.get("/items", function (req, res) {
 
-    let item = req.body;
-
-    console.log(item);
-
-    db.run(`INSERT INTO products (product, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)`,
-        [item['product'], item['origin'], item['best_before_date'], item['amount'], item['image']],
-        function (err, item) {
-            if (err) {
-                res.status(400).send(err);
-            } else {
-                res.status(201).json(item);
-                // res.send(row);
-            }
-        })
+    db.all("SELECT id, product, origin, best_before_date, amount, image FROM products", function (err, rows) {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.json(rows);
+        }
+    })
 });
 
-// UPDATING ITEM
+// Update
 router.put("/items", function (req, res) {
 
     let item = req.body;
 
-    // is it fine if we require all items info to be requested with ??? 
     db.run(`UPDATE products SET product=?, origin=?, best_before_date=?, amount=?,image=? WHERE id=?`,
         [item['product'], item['origin'], item['best_before_date'], item['amount'], item['image'], item['id']],
         function (err, item) {
             if (err) console.log(err);
             if (err) {
                 res.status(400).send(err);
-            } else if (item.n === 0) {
-                res.sendStatus(404);
             } else {
                 res.sendStatus(204);
-                //  Might want to respond with the updated version of item
+                //  Might want to respond with the URI of updated item
             }
         })
 });
 
-// DELETING ITEM
+// Delete
 router.delete("/items/:id", function (req, res) {
 
     let id = req.params.id
 
-    db.run("DELETE FROM products WHERE id= ?", [id], function (err, item) {
+    db.run("DELETE FROM products WHERE id=" + id, function (err, item) {
         if (err) {
-            console.log(err);
             res.status(400).send(err);
         } else if (item.n === 0) {
             res.sendStatus(404);
         } else {
             res.sendStatus(204);
-            // Might want to inform somehow item was deleted
         }
     })
 });
 
-// RESET ITEM - doesn't work
-router.delete("/reset", function (req, res) {
-
-    console.log("Hitting it")
+// Reset db
+router.get("/reset", function (req, res) {
 
     db.run("DELETE  FROM products", function (err) {
-        if (err) console.log(err);
-        // ERROR HANDLING
-        res.send("Table reset")
+        if (err) {
+            res.status(400).send(err);
+            console.log(err);
+        } else {
+            res.sendStatus(204);
+            // check if reset should be safe, there is console.log after calling following function;
+            my_database('./products.db');
+        }
     })
 });
 
-app.use("/api", router);
-
 // ************************************************************************************************ //
 
-
-// LISTENING ON PORT: 3000
+// INIT ROUTERS & LISTENING PORT
+app.use("/api", router);
 app.listen(3000);
-console.log("Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/api/item");
+console.log("Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/api/items");
 
 
 // INIT DATABASE
 function my_database(filename) {
-    // Connects to db by opening filename, creates filename if it does not exist:
+    // Connects to db by opening filename, creates filename if it does not exist
     let db = new sqlite.Database(filename, (err) => {
         if (err) {
             console.error(err.message);
@@ -134,7 +133,7 @@ function my_database(filename) {
         console.log('Connected to the products database.');
     });
 
-    // Creates products table if it does not exist already:
+    // Creates products table if it does not exist already
     db.serialize(() => {
         db.run(`
         	CREATE TABLE IF NOT EXISTS products
@@ -151,9 +150,9 @@ function my_database(filename) {
                 // Generates first item in the products table
                 db.run(`INSERT INTO products (product, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)`,
                     ["Apples", "The Netherlands", "November 2019", "100kg", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Apples.jpg/512px-Apples.jpg"]);
-                console.log('Inserted dummy Apples entry into empty product database');
+                // console.log('Inserted dummy Apples entry into empty product database');
             } else {
-                console.log("Database already contains", result[0].count, " item(s) at startup.");
+                // console.log("Database already contains", result[0].count, " item(s) at startup.");
             }
         });
     });
